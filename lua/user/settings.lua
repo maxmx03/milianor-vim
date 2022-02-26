de1fea6f3432696efe0ee19cbe23521e452f38ab
max <milianordelcanto@gmail.com>
2022-02-26 04:21:35 -0300

    update settings

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -15,9 +15,6 @@ vim.g.dashboard_default_executive = 'telescope'
 vim.o.termguicolors = true
 
 vim.cmd([[
-set t_8f=^[[38;2;%lu;%lu;%lum
-set t_8b=^[[48;2;%lu;%lu;%lum
-
 if has('python')
   set pyx=2
 elseif has('python3') 
