9dbb7230b1340a49fdc638b39b686e8a7df27463
max <milianordelcanto@gmail.com>
2022-02-26 22:58:54 -0300

    fix grammar

--- after/plugin/autotag.lua
+++ after/plugin/autotag.lua
@@ -3,5 +3,5 @@ local success, autotag = pcall(require, 'nvim-ts-autotag')
 if success then
   autotag.setup()
 else
-  vim.notify('nvim-autotagis not working', 'error')
+  vim.notify('nvim-autotag is not working', 'error')
 end
