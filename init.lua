1675e9de23a83e250dfc1e263b40adfca8f94d37
Max Miliano Del Canto Quezada <milianor@pop-os.localdomain>
2022-05-02 15:56:38 -0300

    update devicons config

--- init.lua
+++ init.lua
@@ -37,7 +37,7 @@ if success then
       italic_keywords = true,
       italic_functions = false,
       italic_variables = false,
-      lualine = 'default', -- evil_lualine, bubbles, slanted, default
+      lualine = 'bubbles', -- evil_lualine, bubbles, slanted, default
     },
     keymapping = function()
       local keymap = function(key, map, opt)
