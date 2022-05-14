7cd3d740fba9c51fc6c61af5559b4d3723241ff3
Max Miliano <milianordelcanto@gmail.com>
2022-05-13 21:08:34 -0300

    removed colorschemes plugins

--- init.lua
+++ init.lua
@@ -9,45 +9,45 @@ if not success then
 end
 
 -- lua/user/theme.lua
--- user.theme.colorscheme = 'kanagawa' -- tokyonight, solarized, kanagawa
--- user.theme.transparent = false
--- user.theme.lualine = 'evil_lualine' -- default, evil_lualine, bubbles, slanted
--- user.theme.style = 'night' -- tokyonight: night, storm
--- user.theme.sidebar = 'left'
--- user.theme.italic_comments = true
--- user.theme.italic_keywords = true
--- user.theme.italic_functions = false
--- user.theme.italic_variables = false
+user.theme.colorscheme = 'kanagawa' -- tokyonight, solarized, kanagawa
+user.theme.transparent = false
+user.theme.lualine = 'evil_lualine' -- default, evil_lualine, bubbles, slanted
+user.theme.style = 'night' -- tokyonight: night, storm
+user.theme.sidebar = 'left'
+user.theme.italic_comments = true
+user.theme.italic_keywords = true
+user.theme.italic_functions = false
+user.theme.italic_variables = false
 
 -- lua/user/langs
--- user.langs = 'all'
+user.langs = 'all'
 
 -- lua/user/formatting
--- user.formatting = {
---   'prettier',
---   'stylua',
--- }
+user.formatting = {
+  'prettier',
+  'stylua',
+}
 
 -- lua/user/diagnostics
--- user.diagnostics = {
---   'eslint',
---   'codespell',
--- }
---
+user.diagnostics = {
+  'eslint',
+  'codespell',
+}
+
 -- lua/user/servers.lua
--- user.servers = {
---   'pylsp',
---   'tsserver',
---   'sumneko_lua',
---   'prismals',
---   'graphql',
---   'cssls',
---   'html',
---   'vuels',
---   'volar',
---   'emmet_ls',
---   'tailwindcss',
---   'svelte',
--- }
+user.servers = {
+  'pylsp',
+  'tsserver',
+  'sumneko_lua',
+  'prismals',
+  'graphql',
+  'cssls',
+  'html',
+  'vuels',
+  'volar',
+  'emmet_ls',
+  'tailwindcss',
+  'svelte',
+}
 
 require('theme.colorschemes.init')
