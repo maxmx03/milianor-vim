806ec20a4a246ce385616a415a6e5cf3d7153832
Max Miliano <milianordelcanto@gmail.com>
2022-05-21 13:48:24 -0300

    update winbar

--- lua/core/winbar.lua
+++ lua/core/winbar.lua
@@ -1,7 +1,36 @@
 -- CREDITS: https://github.com/ChristianChiarulli
 local gps = require('nvim-gps')
+local icons = require('utils').icons()
 
-gps.setup()
+gps.setup({
+  icons = {
+    ['class-name'] = '%#CmpItemKindClass#' .. icons.kind.Class .. '%*' .. '', -- Classes and class-like objects
+    ['function-name'] = '%#CmpItemKindFunction#' .. icons.kind.Function .. '%*' .. '', -- Functions
+    ['method-name'] = '%#CmpItemKindMethod#' .. icons.kind.Method .. '%*' .. '', -- Methods (functions inside class-like objects)
+    ['container-name'] = '%#CmpItemKindProperty#' .. icons.type.Object .. '%*' .. '', -- Containers (example: lua tables)
+    ['tag-name'] = '%#CmpItemKindKeyword#' .. icons.misc.Tag .. '%*' .. ' ', -- Tags (example: html tags)
+    ['mapping-name'] = '%#CmpItemKindProperty#' .. icons.type.Object .. '%*' .. '',
+    ['sequence-name'] = '%CmpItemKindProperty#' .. icons.type.Array .. '%*' .. '',
+    ['null-name'] = '%CmpItemKindField#' .. icons.kind.Field .. '%*' .. '',
+    ['boolean-name'] = '%CmpItemKindValue#' .. icons.type.Boolean .. '%*' .. '',
+    ['integer-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. '',
+    ['float-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. '',
+    ['string-name'] = '%CmpItemKindValue#' .. icons.type.String .. '%*' .. '',
+    ['array-name'] = '%CmpItemKindProperty#' .. icons.type.Array .. '%*' .. '',
+    ['object-name'] = '%CmpItemKindProperty#' .. icons.type.Object .. '%*' .. '',
+    ['number-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. '',
+    ['table-name'] = '%CmpItemKindProperty#' .. icons.ui.Table .. '%*' .. '',
+    ['date-name'] = '%CmpItemKindValue#' .. icons.ui.Calendar .. '%*' .. '',
+    ['date-time-name'] = '%CmpItemKindValue#' .. icons.ui.Table .. '%*' .. '',
+    ['inline-table-name'] = '%CmpItemKindProperty#' .. icons.ui.Calendar .. '%*' .. '',
+    ['time-name'] = '%CmpItemKindValue#' .. icons.misc.Watch .. '%*' .. '',
+    ['module-name'] = '%CmpItemKindModule#' .. icons.kind.Module .. '%*' .. '',
+  },
+  separator = ' ' .. icons.ui.ChevronRight .. ' ',
+  depth = 0,
+  depth_limit_indicator = '..',
+  text_hl = 'LineNr',
+})
 
 local function isempty(s)
   return s == nil or s == ''
@@ -44,8 +73,6 @@ local function get_gps()
   if not status_ok then
     return
   end
-
-  local icons = require('utils').icons()
 
   if not gps.is_available() then -- Returns boolean value indicating whether a output can be provided
     return
