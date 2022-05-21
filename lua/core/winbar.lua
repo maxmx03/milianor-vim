ba90230433ea96dc21f4c9b594c58ede05886d67
Max Miliano <milianordelcanto@gmail.com>
2022-05-21 19:52:57 -0300

    fix spacing

--- lua/core/winbar.lua
+++ lua/core/winbar.lua
@@ -4,27 +4,27 @@ local icons = require('utils').icons()
 
 gps.setup({
   icons = {
-    ['class-name'] = '%#CmpItemKindClass#' .. icons.kind.Class .. '%*' .. '', -- Classes and class-like objects
-    ['function-name'] = '%#CmpItemKindFunction#' .. icons.kind.Function .. '%*' .. '', -- Functions
-    ['method-name'] = '%#CmpItemKindMethod#' .. icons.kind.Method .. '%*' .. '', -- Methods (functions inside class-like objects)
-    ['container-name'] = '%#CmpItemKindProperty#' .. icons.type.Object .. '%*' .. '', -- Containers (example: lua tables)
+    ['class-name'] = '%#CmpItemKindClass#' .. icons.kind.Class .. '%*' .. ' ', -- Classes and class-like objects
+    ['function-name'] = '%#CmpItemKindFunction#' .. icons.kind.Function .. '%*' .. ' ', -- Functions
+    ['method-name'] = '%#CmpItemKindMethod#' .. icons.kind.Method .. '%*' .. ' ', -- Methods (functions inside class-like objects)
+    ['container-name'] = '%#CmpItemKindProperty#' .. icons.type.Object .. '%*' .. ' ', -- Containers (example: lua tables)
     ['tag-name'] = '%#CmpItemKindKeyword#' .. icons.misc.Tag .. '%*' .. ' ', -- Tags (example: html tags)
-    ['mapping-name'] = '%#CmpItemKindProperty#' .. icons.type.Object .. '%*' .. '',
-    ['sequence-name'] = '%CmpItemKindProperty#' .. icons.type.Array .. '%*' .. '',
-    ['null-name'] = '%CmpItemKindField#' .. icons.kind.Field .. '%*' .. '',
-    ['boolean-name'] = '%CmpItemKindValue#' .. icons.type.Boolean .. '%*' .. '',
-    ['integer-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. '',
-    ['float-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. '',
-    ['string-name'] = '%CmpItemKindValue#' .. icons.type.String .. '%*' .. '',
-    ['array-name'] = '%CmpItemKindProperty#' .. icons.type.Array .. '%*' .. '',
-    ['object-name'] = '%CmpItemKindProperty#' .. icons.type.Object .. '%*' .. '',
-    ['number-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. '',
-    ['table-name'] = '%CmpItemKindProperty#' .. icons.ui.Table .. '%*' .. '',
-    ['date-name'] = '%CmpItemKindValue#' .. icons.ui.Calendar .. '%*' .. '',
-    ['date-time-name'] = '%CmpItemKindValue#' .. icons.ui.Table .. '%*' .. '',
-    ['inline-table-name'] = '%CmpItemKindProperty#' .. icons.ui.Calendar .. '%*' .. '',
-    ['time-name'] = '%CmpItemKindValue#' .. icons.misc.Watch .. '%*' .. '',
-    ['module-name'] = '%CmpItemKindModule#' .. icons.kind.Module .. '%*' .. '',
+    ['mapping-name'] = '%#CmpItemKindProperty#' .. icons.type.Object .. '%*' .. ' ',
+    ['sequence-name'] = '%CmpItemKindProperty#' .. icons.type.Array .. '%*' .. ' ',
+    ['null-name'] = '%CmpItemKindField#' .. icons.kind.Field .. '%*' .. ' ',
+    ['boolean-name'] = '%CmpItemKindValue#' .. icons.type.Boolean .. '%*' .. ' ',
+    ['integer-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. ' ',
+    ['float-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. ' ',
+    ['string-name'] = '%CmpItemKindValue#' .. icons.type.String .. '%*' .. ' ',
+    ['array-name'] = '%CmpItemKindProperty#' .. icons.type.Array .. '%*' .. ' ',
+    ['object-name'] = '%CmpItemKindProperty#' .. icons.type.Object .. '%*' .. ' ',
+    ['number-name'] = '%CmpItemKindValue#' .. icons.type.Number .. '%*' .. ' ',
+    ['table-name'] = '%CmpItemKindProperty#' .. icons.ui.Table .. '%*' .. ' ',
+    ['date-name'] = '%CmpItemKindValue#' .. icons.ui.Calendar .. '%*' .. ' ',
+    ['date-time-name'] = '%CmpItemKindValue#' .. icons.ui.Table .. '%*' .. ' ',
+    ['inline-table-name'] = '%CmpItemKindProperty#' .. icons.ui.Calendar .. '%*' .. ' ',
+    ['time-name'] = '%CmpItemKindValue#' .. icons.misc.Watch .. '%*' .. ' ',
+    ['module-name'] = '%CmpItemKindModule#' .. icons.kind.Module .. '%*' .. ' ',
   },
   separator = ' ' .. icons.ui.ChevronRight .. ' ',
   depth = 0,
@@ -84,7 +84,7 @@ local function get_gps()
     return ''
   else
     if not isempty(gps_location) then
-      return retval .. ' ' .. icons.ui.ChevronRight .. ' ' .. gps_location
+      return string.format('%s %s %s', retval, icons.ui.ChevronRight, gps_location)
     else
       return retval
     end
