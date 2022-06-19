c3f3bddc640954c759eabd34871d2fd1a519497f
maxmx03 <milianordelcanto@gmail.com>
2022-06-19 15:26:08 -0300

    changes: cmp, settings

--- lua/core/cmp.lua
+++ lua/core/cmp.lua
@@ -15,7 +15,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm { select = true },
+    ['<CR>'] = cmp.mapping.confirm { select = false },
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
