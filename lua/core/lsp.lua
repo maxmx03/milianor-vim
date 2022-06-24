5fe8ca73e8a70df85b3456332605eb910c1121d7
maxmx03 <milianordelcanto@gmail.com>
2022-06-24 07:49:42 -0300

    update lspconfig

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -3,12 +3,6 @@ local lspconfig = require 'lspconfig'
 local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  vim.diagnostic.config {
-    virtual_text = {
-      prefix = '',
-    },
-  }
-
   vim.cmd [[
     augroup LspFormatting
       autocmd! * <buffer>
@@ -17,48 +11,42 @@ local on_attach = function(_, bufnr)
   ]]
 end
 
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = '',
+  },
+}
+
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lsp_flags = {
+  debounce_text_changes = 150,
+}
+local config = {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  flags = lsp_flags,
+}
 
-local runtime_path = vim.split(package.path, ';')
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
-
-local servers = user.servers
-for _, lsp in pairs(servers) do
-  local config = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
-    },
-  }
-
-  if lsp == 'sumneko_lua' and user.luadev then
+for _, server in pairs(user.servers) do
+  if server == 'sumneko_lua' and user.luadev then
     local luadev = require 'lua-dev'
 
     config = luadev.setup {
       library = {
-        vimruntime = true, -- runtime path
-        types = true, -- full signature, docs and completion of vim.api, vim.treesitter, vim.lsp and others
-        plugins = true, -- installed opt or start plugins in packpath
-        -- you can also specify the list of plugins to make available as a workspace library
-        -- plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
+        vimruntime = true,
+        types = true,
+        plugins = true,
       },
-      runtime_path = false, -- enable this to get completion in require strings. Slow!
-      -- pass any additional options that will be merged in the final lsp config
+      runtime_path = false,
       lspconfig = {
         on_attach = on_attach,
         capabilities = capabilities,
-        flags = {
-          -- This will be the default in neovim 0.7+
-          debounce_text_changes = 150,
-        },
+        flags = lsp_flags,
       },
     }
   end
 
-  lspconfig[lsp].setup(config)
+  lspconfig[server].setup(config)
 end
 
 local success, dartls = pcall(require, 'flutter-tools')
