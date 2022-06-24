a97a83c15b9992170f54255e49e4c38be75c3362
maxmx03 <milianordelcanto@gmail.com>
2022-06-24 08:06:39 -0300

    revert changes for init.lua

--- init.lua
+++ init.lua
@@ -1,65 +1,118 @@
 require 'user.init'
 
-user.theme.colorscheme = 'solarized'
-user.theme.lualine = 'default'
-user.theme.sidebar = 'bottom'
-user.theme.rainbow = true
-user.langs = {
-  'bash',
-  'c',
-  'c_sharp',
-  'cpp',
-  'css',
-  'dart',
-  'fish',
-  'go',
-  'graphql',
-  'html',
-  'java',
-  'javascript',
-  'json',
-  'kotlin',
-  'lua',
-  'markdown',
-  'php',
-  'prisma',
-  'python',
-  'regex',
-  'scss',
-  'svelte',
-  'tsx',
-  'typescript',
-  'vim',
-  'vue',
-  'yaml',
-}
+-- TODO: Tutorial - complete the todolist bellow
 
-user.formatting = {
-  'prettier',
-  'stylua',
-}
+-- TODO: Press space to see the keymapping or go to lua/user/keymapping.lua, press <space>ff to locate that file.
 
-user.diagnostics = {
-  'eslint',
-}
+-- TODO: change theme
+-- user.luadev = false
+-- user.theme.colorscheme = 'kanagawa' -- solarized
+-- user.theme.lualine = 'bubbles' -- default, evil_lualine, bubbles, slanted
+-- user.theme.sidebar = 'bottom' -- right, left, bottom, toplevel
+-- user.theme.rainbow = true -- enable rainbow plugin (parentheses colors)
 
-user.servers = {
-  'pylsp',
-  'tsserver',
-  'sumneko_lua',
-  'prismals',
-  'graphql',
-  'cssls',
-  'html',
-  'vuels',
-  'volar',
-  'emmet_ls',
-  'tailwindcss',
-  'svelte',
-}
+-- TODO: Change lualine colors, to see the available pallets to go lua/theme/lualine/colors.lua
+-- user.theme.lualine_colors = {
+--   bg = '#16161D',
+--   fg = '#C8C093',
+--   yellow = '#938056',
+--   cyan = '#6A9589',
+--   darkblue = '#7E9CD8',
+--   green = '#98c379',
+--   orange = '#FFA066',
+--   violet = '#938AA9',
+--   magenta = '#D16D9E',
+--   blue = '#658594',
+--   red = '#E82424',
+--   grey = '#54546D',
+-- }
 
-user.plugins = {
-  'ggandor/lightspeed.nvim',
-}
+-- TODO: Change rainbow colors, to see the available pallets go to lua/theme/rainbow/colors.lua
+-- user.theme.rainbow_colors = {
+--   '#C8C093',
+--   '#938056',
+--   '#6A9589',
+--   '#98c379',
+--   '#FFA066',
+--   '#D16D9E',
+--   '#658594',
+--   '#E82424',
+-- }
 
-vim.g.transparent_background = true
+-- TODO: pick your programm langs to improve hignlighting
+-- user.langs = {
+--   'bash',
+--   'c',
+--   'c_sharp',
+--   'cpp',
+--   'css',
+--   'dart',
+--   'fish',
+--   'go',
+--   'graphql',
+--   'html',
+--   'java',
+--   'javascript',
+--   'json',
+--   'kotlin',
+--   'lua',
+--   'markdown',
+--   'php',
+--   'prisma',
+--   'python',
+--   'regex',
+--   'scss',
+--   'svelte',
+--   'tsx',
+--   'typescript',
+--   'vim',
+--   'vue',
+--   'yaml',
+-- }
+
+-- TODO: pick your formatters to use, to see more options go to lua/user/formatting.lua
+-- user.formatting = {
+--   'prettier',
+--   'stylua',
+-- }
+
+-- TODO: pick your diagnostics to use, to see more options go to lua/user/diagnostics.lua
+-- user.diagnostics = {
+--   'eslint',
+-- }
+
+-- TODO: pick your servers to use, to see more options go to lua/user/server.lua
+-- user.servers = {
+--   'pylsp',
+--   'tsserver',
+--   'sumneko_lua',
+--   'prismals',
+--   'graphql',
+--   'cssls',
+--   'html',
+--   'vuels',
+--   'volar',
+--   'emmet_ls',
+--   'tailwindcss',
+--   'svelte',
+-- }
+
+-- TODO: Add more plugins, docs: https://github.com/wbthomason/packer.nvim
+-- user.plugins = {
+--   'justinmk/vim-sneak',
+--   'rebelot/kanagawa.nvim',
+--   {
+--     'akinsho/flutter-tools.nvim',
+--     requires = { 'nvim-lua/plenary.nvim', 'thosakwe/vim-flutter' },
+--   },
+-- }
+
+-- TODO: Insert new keymapping
+-- user.keymapping['F'] = {
+--   name = 'FLUTTER',
+--   r = { ':FlutterRun<cr>', 'Flutter run' },
+--   R = { ':FlutterHotRestart<cr>', 'Flutter restart' },
+--   q = { ':FlutterQuit<cr>', 'Flutter quit' },
+--   v = { ':FlutterVisualDebug<cr>', 'Flutter visual debug' },
+--   o = { ':FlutterOutlineToggle<cr>', 'Flutter outline debug' },
+-- }
