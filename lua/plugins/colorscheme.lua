7ef4239f0f5517c50c35cc5b4a935bde38f55d82
Max Miliano <milianordelcanto@gmail.com>
2023-04-18 12:33:10 -0300

    rm draculaprocolors from colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,17 +7,8 @@ return {
       local dracula = require 'dracula'
 
       dracula.setup {
-        colors = {
-          bg = '#22212C',
-          float_bg = '#17161D',
-          comment = '#7970A9',
-          currentline = '#454158',
-          selection = '#454158',
-        },
         override = function(c)
           return {
-            MiniTablineCurrent = { fg = c.purple, bg = c.blended_purple },
-            MiniTablineVisible = { link = 'MiniTablineCurrent' },
             CmpItemKindText = { reverse = true },
             CmpItemKindMethod = { reverse = true },
             CmpItemKindFunction = { reverse = true },
