abb3a855e62f108fd17f0966bb1de6c0dd18e6c9
Max Del Canto <milianordelcanto@gmail.com>
2023-11-03 12:02:01 -0300

    refactor: nvim-tree, indentblankline

--- lua/plugins/indentblankline.lua
+++ lua/plugins/indentblankline.lua
@@ -1,6 +1,7 @@
 return {
   {
     'lukas-reineke/indent-blankline.nvim',
+    commit = 'b2e15a8a2a7c6c06fc4f405844b561fffa123c42',
     config = function()
       vim.opt.list = true
       vim.opt.listchars:append 'space:⋅'
@@ -13,7 +14,7 @@ return {
       }
 
       vim.g.indent_blankline_filetype_exclude =
-        { 'dashboard', 'lspinfo', 'packer', 'checkhealth', 'help', 'man', 'NvimTree' }
+      { 'dashboard', 'lspinfo', 'packer', 'checkhealth', 'help', 'man', 'NvimTree' }
     end,
   },
 }
